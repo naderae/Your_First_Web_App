d18d101dba89b45cb7a7250984a0b826373a512e
naderae <naderabouezze93@gmail.com>
2017-07-12 18:59:56 -0400

    last

--- server.rb
+++ server.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
 
 get'/portfolio' do
   erb :gallery
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb:me
+end
+
+get '/favourites' do
+  @fav_links = ['google.com', 'nba.com', 'bleacherreport.com', 'goal.com', 'brobible.com']
+  erb :fav_links
 end
